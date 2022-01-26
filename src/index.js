8317e65b5d6e6a8f99d803955392296487fb6ac4
NickNych <nikolpaladiy@gmail.com>
2022-01-26 11:53:28 +0200

    fix: fixed functions in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function toReadable(number) {
-	return [`Zero`, `One`, `Two`, `Three`, `Four`, `Five`, `Six`, `Seven`, `Eight`, `Nine` `Ten`][number]
+    return [`Zero`, `One`, `Two`, `Three`, `Four`, `Five`, `Six`, `Seven`, `Eight`, `Nine`, `Ten`][number]
 
 }
