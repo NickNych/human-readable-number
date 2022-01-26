856533dde00cf932a1b98a3d8c683fd83deb2c76
NickNych <nikolpaladiy@gmail.com>
2022-01-26 15:15:26 +0200

    fix: fixed functions in index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports = function toReadable(number) {
         `fifty one`, `fifty two`, `fifty three`, `fifty four`, `fifty five`, `fifty six`, `fifty seven`, `fifty eight`, `fifty nine`, `sixty`,
         `sixty one`, `sixty two`, `sixty three`, `sixty four`, `sixty five`, `sixty six`, `sixty seven`, `sixty eight`, `sixty nine`, `seventy`,
         `seventy one`, `seventy two`, `seventy three`, `seventy four`, `seventy five`, `seventy six`, `seventy seven`, `seventy eight`, `seventy nine`, `eighty`,
-        `eighty one`, `eighty two`, `eighty three`, `eighty four`, `eighty five`, `six`, `eighty seven`, `eighty eight`, `eighty nine`, `ninety`,
+        `eighty one`, `eighty two`, `eighty three`, `eighty four`, `eighty five`, `eighty six`, `eighty seven`, `eighty eight`, `eighty nine`, `ninety`,
         `ninety one`, `ninety two`, `ninety three`, `ninety four`, `ninety five`, `ninety six`, `ninety seven`, `ninety eight`, `ninety nine`, `one hundred`,
         `one hundred one`, `one hundred two`, `one hundred three`, `one hundred four`, `one hundred five`, `one hundred six`, `one hundred seven`, `one hundred eight`, `one hundred nine`, `one hundred ten`,
         `one hundred eleven`, `one hundred twelve`, `one hundred thirteen`, `one hundred fourteen`, `one hundred fifteen`, `one hundred sixteen`, `one hundred seventeen`, `one hundred eighteen`, `one hundred nineteen`, `one hundred twenty`,
