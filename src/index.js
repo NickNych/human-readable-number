a07b28723faf2378adc00bf27a2a997acb07f959
NickNych <nikolpaladiy@gmail.com>
2022-01-26 11:48:33 +0200

    add: new functional in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+	return [`Zero`, `One`, `Two`, `Three`, `Four`, `Five`, `Six`, `Seven`, `Eight`, `Nine` `Ten`][number]
+
 }
